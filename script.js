4ea9b906c6b8e676e5f2a7640c3349415b3ce204
Joshua10roys <95379099+Joshua10roys@users.noreply.github.com>
2022-01-23 20:08:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var submit = document.querySelector("#submit");
+
+submit.addEventListener(("click"), function (event) {
+    event.preventDefault();
+    var fname = document.getElementById("first-name").value;
+    var lname = document.getElementById("last-name").value;
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    var address = document.getElementById("address").value;
+    var state = document.getElementById("state").value;
+    var country = document.getElementById("country").value;
+    var pincode = document.getElementById("pincode").value;
+
+    var table = document.querySelector("#table-body");
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = fname;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = lname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = gender;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = address;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = state;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = country;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = pincode;
+
+    document.getElementById("form").reset()
+});
